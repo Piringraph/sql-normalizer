b0896d8403e12a52f89dc27a744e9fe0f239685b
piringraph <chuckdanorris123@gmail.com>
2022-07-03 21:42:39 -0400

    fixed syntax errors

--- main.go
+++ main.go
@@ -1,6 +1,50 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "Felix"
+	password = "123"
+	dbname   = "gnema"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user,
+		password)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	resetDB(db, dbname)
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func normalize(phone string) string {
 	var buf bytes.Buffer
